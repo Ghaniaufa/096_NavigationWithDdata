bb9eb091f75ae16b9092127dd6bb9efc815045bf
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-11-14 13:54:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/eslilin/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.eslilin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eslilin.ui.theme.EsLilinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EsLilinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EsLilinApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EsLilinTheme {
+        EsLilinApp()
+    }
+}
---
+++ app/src/main/java/com/example/eslilin/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.eslilin.data
+
+import com.example.eslilin.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
