61e9d39154a0c922ba288ef1eea06225f19f6133
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-11-14 16:15:01 +0700

    Create function HalamanForm for variable namaTxt,alamatTxt, and telponTxt

--- app/src/main/java/com/example/eslilin/HalamanForm.kt
+++ app/src/main/java/com/example/eslilin/HalamanForm.kt
@@ -1,2 +1,22 @@
 package com.example.eslilin
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick: (MutableList<String>) -> Unit
+){
+    var namaTxt by remember{
+        mutableStateOf("")
+    }
+    var alamatTxt by remember{
+        mutableStateOf("")
+    }
+    var telponTxt by remember{
+        mutableStateOf("")
+    }
+}
--- app/src/main/java/com/example/eslilin/HalamanHome.kt
+++ app/src/main/java/com/example/eslilin/HalamanHome.kt
@@ -90,6 +90,6 @@ fun HalamanHome(
 @Composable
 fun PreviewHalamanHome(){
     EsLilinTheme {
-        HalamanHome(onNextButtonClicked = {})
+        EsLilinApp()
     }
 }
