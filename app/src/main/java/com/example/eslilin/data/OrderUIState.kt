fb3cc128ec465520a904926769649ae86d6deb1e
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-11-14 15:41:27 +0700

    Add new data from Stringxml and OrderUIstate

--- app/src/main/java/com/example/eslilin/data/OrderUIState.kt
+++ app/src/main/java/com/example/eslilin/data/OrderUIState.kt
@@ -3,5 +3,8 @@ package com.example.eslilin.data
 data class OrderUiState(
     val jumlah: Int = 0,
     val rasa: String ="",
-    val harga: String = ""
+    val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val noTelp: String = ""
 )
